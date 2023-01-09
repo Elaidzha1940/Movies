c31c67736bfee3694581089446c0d669cc9e1744
Elaidzha1940 <eli080293@gmail.com>
2023-01-09 22:44:32 +0700

    first commit

---
+++ Movies/Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Elaidzha Shchukin on 09.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
