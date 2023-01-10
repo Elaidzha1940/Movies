cc14c6dee37e39dfc5224a491d64e514b3915c71
Elaidzha1940 <eli080293@gmail.com>
2023-01-10 15:13:44 +0700

    Movies + ViewModel

--- Movies/Movies/ContentView.swift
+++ Movies/Movies/ContentView.swift
@@ -7,60 +7,77 @@
 
 import SwiftUI
 
-struct ContentView: View {
+class ViewModel: ObservableObject {
     
-    class ViewModel: ObservableObject {
-        @Published var image: Image ?
-        
-        func fetchNewImage() {
-            guard let url = URL(string:
-                                    "https://random.imagecdn.app/500/500") else {
-                return
-            }
-            let task = URLSession.shared.data.dataTask(with: url) { data,
-                 _, _ in
-                guard let data = data else { return }
-                
-                DispatchQueue.main.async {
-                    guard let uiImage = UIImage(data: data) else {
-                        return
-                    }
-                    self.image = Image(uiImage: uiImage)
+    @Published var image: Image?
+    
+    func fetchNewImage() {
+        guard let url = URL(string:
+                                "https://random.imagecdn.app/500/500") else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+            guard let data = data else { return }
+            
+            DispatchQueue.main.async {
+                guard let uiImage = UIImage(data: data) else {
+                    return
                 }
+                self.image = Image(uiImage: uiImage)
             }
         }
+        task.resume()
     }
+}
+
+struct ContentView: View {
+    
+    @StateObject var viewModel = ViewModel()
     
     var body: some View {
         
         NavigationView {
-            
             VStack {
                 
                 Spacer()
                 
-                Image(systemName: "photo")
-                    .resizable()
-                    .foregroundColor(.brown)
-                    .frame(width: 400, height: 450)
-                    .padding()
-                
+                if let image = viewModel.image {
+                    ZStack {
+                        
+                        image
+                            .resizable()
+                            .foregroundColor(.brown)
+                            .frame(width: 400, height: 450)
+                            .padding()
+                    }
+                    .frame(width: UIScreen.main.bounds.width / 1.2,
+                           height: UIScreen.main.bounds.width / 1.2)
+                    .background(Color.black)
+                    .cornerRadius(12)
+                } else {
+                    
+                    Image(systemName: "photo")
+                        .resizable()
+                        .foregroundColor(.brown)
+                        .frame(width: 400, height: 450)
+                        .padding()
+                }
                 Spacer()
                 
                 Button {
-                    print()
+                    viewModel.fetchNewImage()
                 } label: {
                     Text("New Movie")
                         .font(.title3)
                         .frame(width: 200, height: 50)
                         .fontWeight(.bold)
                         .foregroundColor(.white)
-                        .background(Color.blue)
+                        .background(Color.brown)
                         .cornerRadius(12)
                         .padding()
                 }
-
             }
+            .background(Color.gray)
             .navigationTitle("Movies")
         }
     }
